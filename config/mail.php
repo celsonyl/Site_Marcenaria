a5791e115de408d9172105370abb33dd67f04036
Celso Antonio Bertolotto Junior <celsonyl@hotmail.com>
2020-07-21 14:50:24 -0300

    TUDO FEIO JA BLZ FALTA SÓ O FRONT END OK

--- config/mail.php
+++ config/mail.php
@@ -36,9 +36,9 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'host' => env('MAIL_HOST'),
+            'port' => env('MAIL_PORT'),
+            'encryption' => env('MAIL_ENCRYPTION'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
@@ -83,10 +83,7 @@ return [
     |
     */
 
-    'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
-    ],
+    'from' => ['address' => 'apenaslebot@hotmail.com', 'name' => 'hawle'],
 
     /*
     |--------------------------------------------------------------------------
